87523b03721ddc95b3e4ffad4821eb456e3dd238
Jake Weinstein <xboxlover360@gmail.com>
2017-06-25 20:03:54 -0400

    angler: remove ENABLE_CPUSETS
    This flag is no longer used.
    
    Change-Id: I95b0635eee14d5d0e7c59b2002d1af59dc2b39ab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,7 +29,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 
-ENABLE_CPUSETS := true
 STRICT_ALIASING := true
 GRAPHITE_OPTS := true
 
