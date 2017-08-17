37d568b96b023fe70913a446ca9957b1b72f5acc
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-08-17 05:54:38 +0200

    Use pathmap for camera

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -50,7 +50,7 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/stack/common \
         frameworks/native/include/media/hardware \
         frameworks/native/include/media/openmax \
-        hardware/qcom/media-caf/msm8994/libstagefrighthw \
+        $(call project-path-for,qcom-media)/libstagefrighthw \
         system/media/camera/include \
         $(LOCAL_PATH)/../mm-image-codec/qexif \
         $(LOCAL_PATH)/../mm-image-codec/qomx_core \
@@ -64,7 +64,7 @@ LOCAL_C_INCLUDES += \
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-#LOCAL_STATIC_LIBRARIES := libqcamera2_util
+# LOCAL_STATIC_LIBRARIES := libqcamera2_util
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 
